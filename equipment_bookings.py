4b180167341ebcadf73ce107d77372a17a3fea71
hahahammond <khammond15@gmail.com>
2018-02-22 10:32:09 -0500

    Updated python file

--- equipment_bookings.py
+++ equipment_bookings.py
@@ -1,4 +1,4 @@
-# Outputs an HTML table listing equipment bookings in your system for the current date
+# Outputs a json file, an HTML file, and a csv file listing equipment bookings in your system for the current date
 
 import  requests, json
 from json2html import *
@@ -9,7 +9,7 @@ test_api_url  =  'https://api2.libcal.com/1.1/equipment/bookings?formAnswers=1'
 
 # Enter your credentials
 client_id  =  '[ENTER CLIENT ID HERE]'
-client_secret  =  '[ENTER CLIENT SECRET HERE]'
+client_secret  = '[ENTER CLIENT SECRET HERE]'
  
 data  =  { 'grant_type' :  'client_credentials' }
  
@@ -23,10 +23,21 @@ api_call_response  =  requests.get(test_api_url, headers = api_call_headers, ver
 my_json = api_call_response.json()
 print(my_json)
 
-## Write json to an html file
+# Write json to json file
+with open("libcal.json","w") as f:
+    json.dump(my_json, f)
+
+# Write json to an html file
 my_html = json2html.convert(json = my_json)
 print(my_html)
 
 my_file = open("libcal.html", 'w')
 my_file.write(my_html)
 my_file.close()
+
+# Create dataframe
+df = pd.DataFrame(my_json)
+print(df)
+
+# Write dataframe to csv file
+df.to_csv("libcal.csv")
